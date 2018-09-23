d39a5dc260b1fc207d5e0f0e0a81dd0e5bde8c87
Dismika <dismikaharish@gmail.com>
2018-09-23 11:51:55 +0530

    add else to checkin method

--- src/hotel/entities/Room.java
+++ src/hotel/entities/Room.java
@@ -73,10 +73,10 @@ public class Room {
 	public void checkin()  {
 		// TODO Auto-generated method stub
 		
-		if (isReady() != true) {
+		if (isReady() != true)
 			throw new Exception("room is not availble");
+		else
 			state = State.OCCUPIED;
-		}
 	}
 
 
